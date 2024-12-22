b694d135b6e213f0546bb4baac6d0f5fe736ad9c
callum <callum.binns@insitetrack.com>
2024-12-22 16:37:18 +0000

    confirmed working part 1, unimplemented part 2 methods

--- src/main/java/com/callumhobby/adventofcode2024day4/AdventOfCode2024Day4.java
+++ src/main/java/com/callumhobby/adventofcode2024day4/AdventOfCode2024Day4.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.callumhobby.adventofcode2024day4;
 
 /**
@@ -9,8 +8,21 @@ package com.callumhobby.adventofcode2024day4;
  * @author CallumBinns
  */
 public class AdventOfCode2024Day4 {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputReader in = new InputReader("src\\main\\java\\com\\callumhobby\\adventofcode2024day4\\Input.txt");
+        CrosswordGrid grid = new CrosswordGrid(in.getLines());
+        Integer[] xMasCount = grid.getInstancesInDiagonals();
+        Integer[] rowsCount = grid.getInstancesInRowsOrColumns(false);
+        xMasCount[0] += rowsCount[0];
+        xMasCount[1] += rowsCount[1];
+        Integer[] columnsCount = grid.getInstancesInRowsOrColumns(true);
+        xMasCount[0] += columnsCount[0];
+        xMasCount[1] += columnsCount[1];
+
+
+        System.out.println("Total count: "+String.valueOf(xMasCount[0]+xMasCount[1]));
+        
+        System.out.println("Total count of mas in x shape: "+String.valueOf(grid.masInXShapeCount()));
     }
 }
--- src/main/java/com/callumhobby/adventofcode2024day4/CrosswordGrid.java
+++ src/main/java/com/callumhobby/adventofcode2024day4/CrosswordGrid.java
@@ -17,9 +17,13 @@ public class CrosswordGrid {
     public char[][] grid;
     private String forwards;
     private String backwards;
+    private Integer gridMaxX;
+    private Integer gridMaxY;
 
     public CrosswordGrid(List<String> input) {
-        this.grid = new char[input.size()][input.getFirst().length()];
+        this.gridMaxX = input.getFirst().length();
+        this.gridMaxY = input.size();
+        this.grid = new char[gridMaxY][gridMaxX];
         for (int i = 0; i < input.size(); i++) {
             for (int j = 0; j < input.get(i).length(); j++) {
                 grid[i][j] = input.get(i).charAt(j);
@@ -28,49 +32,73 @@ public class CrosswordGrid {
         this.forwards = "XMAS";
         this.backwards = "SAMX";
     }
-/**
- * 
- * @param isColumns
- * @return integer array where 0 is forwards count and 1 is backwards count
- */
+
+    /**
+     *
+     * @param isColumns
+     * @return integer array where 0 is forwards count and 1 is backwards count
+     */
     public Integer[] getInstancesInRowsOrColumns(boolean isColumns) {
-        Integer[] xMasCount = {0, 0};
         List<String> linesToCheck = new ArrayList<>();
-        
+
         if (!isColumns) {
             for (int i = 0; i < grid[0].length; i++) {
                 linesToCheck.add("");
                 for (char[] row : grid) {
-                    linesToCheck.set(i,linesToCheck.get(i)+row[i]);
+                    linesToCheck.set(i, linesToCheck.get(i) + row[i]);
                 }
             }
-        }
-        else{
+        } else {
             for (char[] row : grid) {
                 linesToCheck.add(String.valueOf(row));
             }
         }
+
+        return xMasCountLogic(linesToCheck);
+    }
+
+    public Integer[] getInstancesInDiagonals() {
+        List<String> linesToCheck = new ArrayList<>();
+        //Doing this graphically with y=mx+c but bound by the length and width of the grid
+        //positive gradient diagonals loops:
+        for (int c = -gridMaxX; c < gridMaxY; c++) {//loop to iterate through each line (aka every possible y intercept)
+            String line = "";
+            for (int x = 0; x < gridMaxX; x++) {//iterate through each possible x value
+                Integer y = x+c;
+                if (y>=0 && x>=0 && y < gridMaxY) {
+                    line += grid[y][x];
+                } 
+            }
+            linesToCheck.add(line);
+        }
+        //negative gradient diagonals loops:
+        for (int c = 0; c < gridMaxY+gridMaxX; c++) {
+            String line = "";
+            for (int x = 0; x < gridMaxX; x++) {//iterate through each possible x value
+                Integer y = -x+c;
+                if (y>=0 && x>=0 && y < gridMaxY) {
+                   line += grid[y][x]; 
+                }
+                
+            }
+            linesToCheck.add(line);
+        }
         
+
+        return xMasCountLogic(linesToCheck);
+    }
+
+    private Integer[] xMasCountLogic(List<String> linesToCheck) {
+        Integer[] xMasCount = {0, 0};
         for (String string : linesToCheck) {
             Integer initialCount = string.length();
             Integer forwardsMissing = string.replaceAll(forwards, "").length();
             Integer backwardsMissing = string.replaceAll(backwards, "").length();
-            
-            xMasCount[0] += initialCount != forwardsMissing ? (initialCount-forwardsMissing)/forwards.length() : 0;
-            xMasCount[1] += initialCount != backwardsMissing ? (initialCount-backwardsMissing)/backwards.length() : 0;
-            
+
+            xMasCount[0] += initialCount != forwardsMissing ? (initialCount - forwardsMissing) / forwards.length() : 0;
+            xMasCount[1] += initialCount != backwardsMissing ? (initialCount - backwardsMissing) / backwards.length() : 0;
+
         }
-        
-        
-       
-
-        return xMasCount;
-    }
-
-    public Integer[] getInstancesInDiagonals() {
-        Integer[] xMasCount = {0, 0};
-        List<String> linesToCheck = new ArrayList<>();
-        
         return xMasCount;
     }
 }
